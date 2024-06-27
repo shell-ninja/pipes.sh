45e1fda68e03f363d2342792007e2ddfa9767335
me-js-bro <me.js.bro@gmail.com>
2024-06-27 08:43:24 +0600

    test

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ printf "${action} - ${orange}Installing the pipes.sh. It may aks for your root p
 # cloning the repo
 git clone --depth=1 https://github.com/me-js-bro/pipes.sh.git "$HOME/.pipes.sh"
 
-if [[ -d ".pipes.sh" ]]; then
+if [[ -d "$HOME/.pipes.sh" ]]; then
     cd "$HOME/.pipes.sh"
     sudo make install
 fi
 
-if [[ -f "/usr/local/bin/pipes.sh" ]]; then
+if [[ -f "/usr/local/bin/pipes.sh" || -f "$HOME/.local/bin/pipes.sh" ]]; then
     printf "Installed successfully. Now run pipes.sh in your terminal and you will get this...\n"
     pipes.sh
 fi
